53569b77973d522d6dba71ac87757e4e1cd7e0b0
Brian Webster <bwebster@fatcatsoftware.com>
2025-02-28 21:47:13 -0800

    Bump to use Swift 5.9 and minumum macOS 12.0 deployment

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.9
 
 import PackageDescription
 
 let package = Package(
     name: "FSCheckoutSheet",
     platforms: [
-        .macOS(.v10_13),
+        .macOS(.v12),
     ],
     products: [
         .library(name: "FSCheckoutSheet", targets: ["FSCheckoutSheet"])
